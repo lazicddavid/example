7f510951383b227494fd39d209af69d5a09d881a
David Lazic <lazic.ddavid@gmail.com>
2025-08-17 14:23:00 +0200

    added addbtn

--- example.js
+++ example.js
@@ -3,3 +3,5 @@ let dotos = [];
 const form = document.getElementById("todoForm");
 const input = document.getElementById("todoInput");
 const list = document.getElementById("todoList");
+
+addBtn.addEventListener("click", function () {});
